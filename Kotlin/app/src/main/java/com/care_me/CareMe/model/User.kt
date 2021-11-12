9e3f69775a12fd044dea9071efdc57a61357c27c
andreicampigotto <andreicampigotto@gmail.com>
2021-11-12 19:07:44 -0300

    Ajustado o model

---
+++ Kotlin/app/src/main/java/com/care_me/CareMe/model/User.kt
@@ -0,0 +1,21 @@
+package com.care_me.CareMe.model
+
+import java.util.*
+
+data class User(
+    val name: String,
+    val age: Int,
+    val weight: Float,
+    val height: Int,
+    val active: Boolean,
+    val bloodPressure: String
+)
+
+data class Drug(
+    val drugName: String,
+    val continuousUse: Boolean
+)
+
+data class Appointments(
+    val appointment: Date
+)

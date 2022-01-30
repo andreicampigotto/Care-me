d9863447ebdef1bd96fdbb447b901c9bcc96302d
andreicampigotto <andreicampigotto@gmail.com>
2022-01-30 14:22:23 -0300

    adjusted login view

--- Kotlin/app/src/main/java/com/care_me/CareMe/view/ActivityLogin.kt
+++ Kotlin/app/src/main/java/com/care_me/CareMe/view/ActivityLogin.kt
@@ -3,22 +3,18 @@ package com.care_me.CareMe.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.care_me.CareMe.R
 import com.care_me.CareMe.databinding.ActivityLoginBinding
-import com.care_me.CareMe.viewModel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ActivityLogin : AppCompatActivity(R.layout.activity_login) {
 
-    private lateinit var viewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
         setContentView(binding.root)
         openApp()
     }
--- Kotlin/app/src/main/java/com/care_me/CareMe/viewModel/LoginViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.care_me.CareMe.viewModel
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-
-@HiltViewModel
-class LoginViewModel: ViewModel() {
-}

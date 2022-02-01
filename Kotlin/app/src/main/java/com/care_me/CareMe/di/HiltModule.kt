a867b85ccba3eb90db0a9015b1ff61878576d03d
jnmendes-philips <jeffersonnazario.mendes@philips.com>
2022-01-31 22:28:10 -0300

    di module created and appDatabase and appointmentDao created

---
+++ Kotlin/app/src/main/java/com/care_me/CareMe/di/HiltModule.kt
@@ -0,0 +1,20 @@
+package com.care_me.CareMe.di
+
+import android.content.Context
+import com.care_me.CareMe.database.AppDatabase
+import com.care_me.CareMe.database.dao.AppointmentDAO
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+private object HiltModule {
+
+    @Provides
+    fun getAppointmentDao(@ApplicationContext context: Context): AppointmentDAO {
+        return AppDatabase.getDatabase(context).getAppointmentDAO()
+    }
+}

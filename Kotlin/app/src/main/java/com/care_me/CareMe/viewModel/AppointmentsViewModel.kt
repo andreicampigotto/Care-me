6169286004430b65f65841721a39e6ad6129bd4e
jnmendes-philips <jeffersonnazario.mendes@philips.com>
2022-01-31 22:46:02 -0300

    viewModel and repository for appointments created

---
+++ Kotlin/app/src/main/java/com/care_me/CareMe/viewModel/AppointmentsViewModel.kt
@@ -0,0 +1,31 @@
+package com.care_me.CareMe.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.care_me.CareMe.model.Appointments
+import com.care_me.CareMe.repository.AppointmentRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class AppointmentsViewModel @Inject constructor(private val repository: AppointmentRepository) :
+    ViewModel() {
+
+    private val _appointmentList = MutableLiveData<List<Appointments>>()
+    val appointmentList: LiveData<List<Appointments>> = _appointmentList
+
+    fun getAppointments() {
+        viewModelScope.launch {
+            _appointmentList.value = repository.getAppointments()
+        }
+    }
+
+    fun setAppointment(appointments: Appointments) {
+        viewModelScope.launch {
+            repository.setAppointment(appointments)
+        }
+    }
+}
